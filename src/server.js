08a1aef1e51798fc6ddb2ae255450e68c71964a3
derekzx <chinzhixian@gmail.com>
2021-10-20 06:51:52 -0700

    add body parser

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 require('dotenv').config()
 
 const express = require('express')
-const Web3 = require("web3")
-const web3 = new Web3("https://bsc-dataseed.binance.org/");
-const { tokenPricePool , limitOrderPool, stopLossPool } = require('./databaseClient');
+const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
+
+const Web3 = require("web3")
+const web3 = new Web3("https://bsc-dataseed.binance.org/");
+
+const { tokenPricePool , limitOrderPool, stopLossPool } = require('./databaseClient');
+
 const port = process.env.PORT
 
 app.use(cors());
+app.use(bodyParser.json());
 app.options('*', cors())
 
 app.get('/', (req, res) => {
@@ -49,6 +54,7 @@ app.post('/createLimitOrder', async (req, res) => {
     orderStatus: "PENDING",
     orderCode: req.body.ordererAddress + "_" + currentTime,
   }
+  console.log("order logged ", orderData);
   const query = "INSERT INTO " + req.body.tokenOutAddress + "_limitOrder VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
     try {
       const [results, fields] = await limitOrderPool.query(query, Object.values(orderData));
