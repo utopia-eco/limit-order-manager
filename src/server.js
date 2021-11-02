132ff5ee379de65e8c196ae6430d6e066b4a848f
derekzx <chinzhixian@gmail.com>
2021-11-02 08:58:39 -0700

    more logging

--- src/server.js
+++ src/server.js
@@ -41,6 +41,8 @@ app.get('/retrieveLimitBuys/:address/:token', async (req, res) => {
 app.get('/retrievePendingLimitBuys/:token', async (req, res) => {
   const query = "SELECT * FROM " + req.params.token.toLowerCase() + "_limitBuy where orderStatus='PENDING'"
   console.log("databases", process.env.DB_LIMIT_ORDER);
+  console.log("databases2", process.env.DB_LIMIT_SELL);
+  console.log("databases3", process.env.DB_STOP_LOSS);
   console.log("abcde")
     try {
       const [results, fields] = await limitBuyPool.query(query);
